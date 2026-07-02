5b02cd1de2cb668e188cc7f8df9457ecabe92bed
LNS Scripts <152081341+gustavolunas@users.noreply.github.com>
2026-07-02 20:21:06 -0300

    Add files via upload

---
+++ cavebot/imbuing.lua
@@ -0,0 +1,36 @@
+CaveBot.Extensions.TestAction = {}
+
+CaveBot.Extensions.TestAction.setup = function()
+
+  local params = {
+    value = "startAutoBuyMarket()\nreturn true",
+    title = "LNS Functions",
+    description = "Choose an option.",
+    multiline = false,
+
+    examples = {
+      {"Buy Market", "startAutoBuyMarket()\nreturn true"},
+
+      {"Auto Imbuement", "checkerImbuementsList()\nreturn true"},
+    }
+  }
+
+  CaveBot.Editor.registerAction("LNS Func.", function()
+    local value = params.value
+
+    UI.EditorWindow(value, params, function(newText)
+      local focusedAction = CaveBot.actionList:getFocusedChild()
+      local index = CaveBot.actionList:getChildCount()
+
+      if focusedAction then
+        index = CaveBot.actionList:getChildIndex(focusedAction)
+      end
+
+      local widget = CaveBot.addAction("function", newText)
+      CaveBot.actionList:moveChildToIndex(widget, index + 1)
+      CaveBot.actionList:focusChild(widget)
+      CaveBot.save()
+    end)
+  end)
+
+end
