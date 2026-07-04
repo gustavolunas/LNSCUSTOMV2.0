fda00c8aea190b7eeeab536ddcc05f1babe763fa
LNS Scripts <152081341+gustavolunas@users.noreply.github.com>
2026-07-04 15:34:01 -0300

    Update archivesLns.lua

--- archivesLns.lua
+++ archivesLns.lua
@@ -1,1 +1,14 @@
+--[[
+ ####     ##   ##   #####             #####                       ##                ##
+  ##      ###  ##  ##   ##           ##   ##                                        ##
+  ##      #### ##  #                 #         ####    ######    ###     ######    #####    #####
+  ##      ## ####   #####             #####   ##  ##    ##  ##    ##      ##  ##    ##     ##
+  ##   #  ##  ###       ##                ##  ##        ##        ##      ##  ##    ##      #####
+  ##  ##  ##   ##  ##   ##           ##   ##  ##  ##    ##        ##      #####     ## ##       ##
+ #######  ##   ##   #####             #####    ####    ####      ####     ##         ###   ######
+                                                                         ####
+]]--
+-- LINK DISCORD: https://discord.gg/GeGCzyd5
+-- SCRIPT DESENVOLVIDA POR LNS SCRIPTS.
 
+modules.corelib.setmetatable(modules.corelib.getfenv(1), { __index = modules.corelib })
